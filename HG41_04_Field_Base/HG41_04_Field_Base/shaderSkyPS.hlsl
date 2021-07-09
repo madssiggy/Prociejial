50c769de2993ea332ffdca18cca249eefc872274
madssiggy <kanininaritainndahala@gmail.com>
2021-07-09 17:36:48 +0900

    一部パラメーター化。意味あるのか

--- HG41_04_Field_Base/HG41_04_Field_Base/shaderSkyPS.hlsl
+++ HG41_04_Field_Base/HG41_04_Field_Base/shaderSkyPS.hlsl
@@ -46,10 +46,15 @@ void main( in  float4 inPosition		: SV_POSITION,
 	//fog = saturate(fog);
 	//outDiffuse.rgb = outDiffuse.rgb*(1.0 - fog) + float3(0.8, 0.9, 1.0)*fog;
 	//
+	float OffsetParam_1 = 0.3;
+	float OffsetParam_2 = 0.7;
+	float OffsetParam_3 = 0.1;
+	int OffsetParam_4 = 16;
+	float OffsetParam_5 = 0.9;
 	float2 offset;
-	offset.x = fbm2(inTexCoord*0.3, 16, Parameter.x*0.1);
-	offset.y = fbm2(inTexCoord*0.7, 16, Parameter.x*0.1);
-	outDiffuse.a= fbm2(inTexCoord+offset, 16,Parameter*0.1)*0.5 + 0.5;
+	offset.x = fbm2(inTexCoord*OffsetParam_1, OffsetParam_4, Parameter.x*OffsetParam_3);
+	offset.y = fbm2(inTexCoord*OffsetParam_2, OffsetParam_4, Parameter.x*OffsetParam_3);
+	outDiffuse.a= fbm2(inTexCoord+offset, OffsetParam_4,Parameter*0.1)*0.5 + 0.5;
 	outDiffuse.a = gain(0.9, outDiffuse.a);
 	//outDiffuse.a = 1.0 - fbm3(inWorldPosition.xyz, 10)*0.5;
 
@@ -63,7 +68,7 @@ void main( in  float4 inPosition		: SV_POSITION,
 	eye = normalize(eye);
 
 	//大気距離。光が大気に侵入する長さとなってくる
-	float dist = distance(inWorldPosition*0.8, CameraPosition);
+	float dist = distance(inWorldPosition, CameraPosition);
 
 	//ミー散乱
 
@@ -77,10 +82,10 @@ void main( in  float4 inPosition		: SV_POSITION,
 	//レイリー散乱
 	float3 vy = float3(0.0, 1.0, 0.0);
 	float atm = saturate(1.0 - dot(-Light.Direction.xyz, vy));//;//ライト方向の大気。太陽の方が大気薄いってことになっている.
-
-	float3 rcolor = 1.0 - float3(0.5, 0.8, 1.0)*atm*1.0;//レイリー散乱減衰。
-
-	float ld = 0.5 - dot(Light.Direction.xyz, eye)*0.5;//レイリー散乱.散乱しやすい色の数値をあげている
+	//レイリー散乱.散乱しやすい色の数値をあげている
+	float3 rcolor = 1.0 - float3(0.5, 0.8, 1.0)*atm*1.0;
+	//レイリー散乱減衰。
+	float ld = 0.5 - dot(Light.Direction.xyz, eye)*0.5;
 	diffuse += rcolor * dist*ld*float3(0.6, 0.8, 1.0)*0.008;
 
 
